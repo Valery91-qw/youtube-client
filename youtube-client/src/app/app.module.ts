f95950dc67ba7898003eb9ac02c892a1eb360f25
Valery91-qw <vakriv91@gmail.com>
2022-03-23 14:46:24 +0300

    feat: add eslint-airbnb and apply it

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import AppRoutingModule from './app-routing.module';
+import App from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    App.AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [App.AppComponent],
 })
-export class AppModule { }
+
+export default class AppModule { }
