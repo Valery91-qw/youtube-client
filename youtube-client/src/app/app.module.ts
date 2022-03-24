0320dafbc3048541289b77514a3ed3acfed7c5d6
Valery91-qw <vakriv91@gmail.com>
2022-03-24 16:18:24 +0300

    feat: create button component

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from "./common/button/button.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
