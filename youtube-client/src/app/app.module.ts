f1ebd73b3a47b9b838b59ba7bf199c74cdefaee9
Valery91-qw <vakriv91@gmail.com>
2022-03-25 10:23:58 +0300

    feat: create sorting container

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { ButtonComponent } from './common/button/button.component';
 import { SearchingFieldComponent } from './header/searching-field/searching-field.component';
+import { SortingComponent } from "./header/sorting/sorting.component";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SearchingFieldComponent } from './header/searching-field/searching-fiel
     HeaderComponent,
     ButtonComponent,
     SearchingFieldComponent,
+    SortingComponent,
   ],
   imports: [
     BrowserModule,
