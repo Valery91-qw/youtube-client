d09e785aa6f9a1d421a508d5c98928c7233f5dce
Valery91-qw <vakriv91@gmail.com>
2022-04-05 14:12:16 +0300

    feat: delete pipe in filter-block

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -12,7 +12,6 @@ import { LoginFormComponent } from './header/login-form/login-form.component';
 import { SubmitButtonComponent } from './header/searching-field/submit-button/submit-button.component';
 import { LogoComponent } from './header/logo/logo.component';
 import { SettingButtonComponent } from './header/setting-button/setting-button.component';
-import { FilterBlockPipe } from './filter-block/filter-block.pipe';
 
 @NgModule({
   declarations: [
@@ -26,7 +25,6 @@ import { FilterBlockPipe } from './filter-block/filter-block.pipe';
     SubmitButtonComponent,
     LogoComponent,
     SettingButtonComponent,
-    FilterBlockPipe,
   ],
   imports: [
     BrowserModule,
