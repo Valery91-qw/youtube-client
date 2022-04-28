e3286b4cd15e76a4ab4e741e6b21d946e2684c5a
Valery91-qw <vakriv91@gmail.com>
2022-04-28 14:05:47 +0300

    feat: add sorting by views

--- youtube-client/src/app/core/filter-block/pipes/filter.pipe.ts
+++ youtube-client/src/app/core/filter-block/pipes/filter.pipe.ts
@@ -6,17 +6,24 @@ import {IShorCards} from "../../../youtube/main/short-card/short-card.interface"
 })
 
 export class FilterPipe implements PipeTransform {
-  transform(value?: IShorCards[], sortBy?: 'date' | 'likes' | 'text', isAsc?: boolean): IShorCards[] {
+  transform(
+    value?: IShorCards[],
+    sortBy?: 'date' | 'views' | 'text',
+    isAsc?: boolean,
+    searchValue?: string
+  ): IShorCards[] | undefined {
     switch (sortBy) {
-      case "date": {
-        // @ts-ignore
-        return value.sort((cur, next) => {
+      case "date":
+        return value?.sort((cur, next) => {
           const curDateMS = new Date(cur.publishedAt).getTime();
           const nextDateMS = new Date(next.publishedAt).getTime();
           return curDateMS > nextDateMS ? -1 : 1
         });
-      }
-      default: // @ts-ignore
+      case "views" :
+        return value?.sort((cur, nex) => {
+          return cur.likeCount > nex.likeCount ? -1 : 1
+        });
+      default:
         return value
     }
   }
