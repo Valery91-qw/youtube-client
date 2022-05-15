be507d76d3869f06f80e19974052de24c0b1b820
Valery91-qw <vakriv91@gmail.com>
2022-05-15 05:14:49 +0300

    feat: layout Auth page

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { AppService } from "./app.service";
 import { YoutubeModule } from "./youtube/youtube.module";
+import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { YoutubeModule } from "./youtube/youtube.module";
     CoreModule,
     BrowserModule,
     AppRoutingModule,
+    AuthModule,
   ],
   providers: [AppService],
   bootstrap: [AppComponent],
